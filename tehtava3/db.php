1ea3485c45d854365b42190d94e06d21d3795fc9
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2024-12-03 23:56:45 +0200

    Update database connection parameters for production environment

--- tehtava3/db.php
+++ tehtava3/db.php
@@ -1,7 +1,6 @@
 <?php
-// Tietokantayhteyden parametrit
 $dbhost = 'localhost';
 $dbport = '5432';
-$dbname = 'tietokanta_nimi';      // Korvaa omalla tietokantasi nimellä
-$dbuser = 'tietokanta_kayttaja';  // Korvaa omalla käyttäjätunnuksellasi
-$dbpass = 'tietokanta_salasana';  // Korvaa omalla salasanallasi
+$dbname = 'tvnija';
+$dbuser = 'tvnija';
+$dbpass = 'mfNxucJbkt7ZafM';
